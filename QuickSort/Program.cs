f47e07c09de3b32d68bbf8b32070d3ad4e7473ee
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-10-17 19:55:06 +0700

    membuat number of elements in array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -17,6 +17,24 @@ namespace QuickSort
         //Number of elements in array
         private int n;
 
+        void read()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n=========================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n=========================");
+
+
+
         
      }
 }
