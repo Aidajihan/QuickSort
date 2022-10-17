137550c4d113132554103e554fe7aa75f3c886b9
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-10-17 20:21:33 +0700

    swap the element at index i whit the element at index j

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -83,7 +83,13 @@ namespace QuickSort
                 }
                 cmp_count++;
 
-               
+                if (i < j) //if the greater element is on the left of the element
+                {
+                    //swap the element at index i whit the element at index j
+                    swap(i, j);
+                    mov_count++;
+                }
+
             }
 
            
