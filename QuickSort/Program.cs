a631145010c532cb120d85ad5fe9d2ed90ff470b
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-10-17 20:19:32 +0700

    Search for an element less than or rqual to pivot

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -74,6 +74,16 @@ namespace QuickSort
                     cmp_count++;
                 }
                 cmp_count++;
+
+                //Search for an element less than or equal to pivot
+                while ((arr[j] > pivot) && (j >= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+               
             }
 
            
